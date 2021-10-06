9dff509697752389e4d0693631f838a117183028
TheMrWhitee <ing-telemex@yandex.ru>
2021-10-06 19:45:27 +0300

    pagination

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=20,
+    username = models.CharField(max_length=150,
                                 unique=True,
                                 verbose_name='Username')
-    email = models.EmailField(max_length=50)
-    first_name = models.CharField(max_length=20, verbose_name='First name')
-    last_name = models.CharField(max_length=20, verbose_name='Last name')
+    email = models.EmailField(max_length=254)
+    first_name = models.CharField(max_length=150, verbose_name='First name')
+    last_name = models.CharField(max_length=150, verbose_name='Last name')
     is_subscribed = models.BooleanField(default=False, blank=True)
--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -7,7 +7,8 @@ User = get_user_model()
 class CustomUserCreateSerializer(UserCreateSerializer):
     class Meta:
         model = User
-        fields = ('email', 'username', 'first_name', 'last_name', 'password')
+        fields = ('email', 'id', 'username', 'first_name', 'last_name',
+                  'password')
 
 
 class CustomUserSerializer(UserSerializer):
