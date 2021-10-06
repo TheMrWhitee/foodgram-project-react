6bc579313eb2bc570f34818771f513364ca8685a
TheMrWhitee <ing-telemex@yandex.ru>
2021-10-06 22:14:22 +0300

    get recipe

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -40,6 +40,8 @@ class Recipe(models.Model):
     image = models.ImageField(upload_to='recipes/')
     text = models.TextField()
     cooking_time = models.IntegerField()
+    is_favorited = models.BooleanField(default=False)
+    is_in_shopping_cart = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
--- backend/foodgram/recipes/serializers.py
+++ backend/foodgram/recipes/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from .models import Recipe, Tag, Ingredient, Component
+from users.serializers import CustomUserSerializer
 
 
 class ComponentSerializer(serializers.ModelSerializer):
-    # проверить id
     id = serializers.ReadOnlyField(source='ingredient.id')
     name = serializers.ReadOnlyField(source='ingredient.name')
     measurement_unit = serializers.ReadOnlyField(
@@ -30,8 +30,10 @@ class TagSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, read_only=True)
     ingredients = ComponentSerializer(many=True, read_only=True)
+    author = CustomUserSerializer(read_only=True)
 
     class Meta:
         model = Recipe
-        fields = ('id', 'tags', 'author', 'ingredients', 'name', 'image',
-                  'text', 'cooking_time', )
+        fields = ('id', 'tags', 'author', 'ingredients', 'is_favorited',
+                  'is_in_shopping_cart', 'name', 'image', 'text',
+                  'cooking_time', )
