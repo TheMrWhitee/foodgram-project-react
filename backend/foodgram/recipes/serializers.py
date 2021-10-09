fbd26beb68c0e23c1f71935c62f8e383dd97267d
TheMrWhitee <ing-telemex@yandex.ru>
2021-10-09 18:02:38 +0300

    create() method

--- backend/foodgram/recipes/serializers.py
+++ backend/foodgram/recipes/serializers.py
@@ -1,3 +1,4 @@
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
 from users.serializers import CustomUserSerializer
@@ -30,12 +31,33 @@ class TagSerializer(serializers.ModelSerializer):
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    tags = TagSerializer(many=True, read_only=True)
+    tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(),
+                                              many=True)
     ingredients = ComponentSerializer(many=True, read_only=True)
     author = CustomUserSerializer(read_only=True)
+    image = Base64ImageField(max_length=None, use_url=True)
 
     class Meta:
         model = Recipe
         fields = ('id', 'tags', 'author', 'ingredients', 'is_favorited',
                   'is_in_shopping_cart', 'name', 'image', 'text',
                   'cooking_time')
+
+    def create(self, validated_data):
+        tags = validated_data.pop('tags')
+        ingredients = self.initial_data.pop('ingredients')
+        recipe = Recipe.objects.create(**validated_data,
+                                       author=self.context['request'].user)
+        for tag in tags:
+            recipe.tags.add(tag)
+
+        for ingredient in ingredients:
+            ingredient_id = ingredient['id']
+            amount = ingredient['amount']
+            component = Component.objects.create(
+                ingredient=Ingredient.objects.get(pk=ingredient_id),
+                amount=amount
+            )
+            recipe.ingredients.add(component)
+
+        return recipe
