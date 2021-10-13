30fe3fff513bab99829c869b72d7fbade83e5613
TheMrWhitee <ing-telemex@yandex.ru>
2021-10-13 13:18:43 +0300

    Follow destroy

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 from .models import Follow
@@ -17,10 +17,18 @@ class FollowViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
-        user = request.user
         following = User.objects.get(pk=kwargs['id'])
-        Follow.objects.create(user=user, following=following)
+        Follow.objects.create(user=request.user, following=following)
         serializer = FollowSerializer([following],
                                       context={'request': request},
                                       many=True)
         return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        following = User.objects.get(pk=kwargs['id'])
+        instance = Follow.objects.get(user=request.user, following=following)
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.delete()
