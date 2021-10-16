1649f509a14b5aacfa7fec70b9403097f4b9beae
TheMrWhitee <ing-telemex@yandex.ru>
2021-10-16 16:41:41 +0300

    fix permissions

--- backend/foodgram/recipes/permissions.py
+++ backend/foodgram/recipes/permissions.py
@@ -10,7 +10,5 @@ class AdminOrAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if (request.method in ['PUT', 'PATCH', 'DELETE']
                 and not request.user.is_anonymous):
-            return (request.user == obj.author
-                    or request.user.is_superuser
-                    or request.user.is_admin())
+            return request.user == obj.author
         return request.method in permissions.SAFE_METHODS
