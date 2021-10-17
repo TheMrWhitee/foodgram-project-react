c13825a6746ffd5285518c12ef4cdd533c4746a9
TheMrWhitee <ing-telemex@yandex.ru>
2021-10-17 12:56:31 +0300

    bug fix

--- backend/foodgram/recipes/filters.py
+++ backend/foodgram/recipes/filters.py
@@ -4,7 +4,7 @@ from .models import Recipe
 
 
 class RecipeFilter(filters.FilterSet):
-    tags = filters.AllValuesMultipleFilter(field_name='tags__name')
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
     is_favorited = filters.BooleanFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='get_is_in_shopping_cart'
