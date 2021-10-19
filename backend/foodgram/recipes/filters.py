1faeae87728ef461493ce9d0e72a4d4ca4fad259
TheMrWhitee <ing-telemex@yandex.ru>
2021-10-19 18:53:50 +0300

    filter complete

--- backend/foodgram/recipes/filters.py
+++ backend/foodgram/recipes/filters.py
@@ -5,25 +5,21 @@ from .models import Ingredient, Recipe
 
 class RecipeFilter(filters.FilterSet):
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = filters.BooleanFilter(method='get_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
-        method='get_is_in_shopping_cart'
-    )
+    is_favorited = filters.BooleanFilter(method='get_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(method='get_shopping_cart')
 
     class Meta:
         model = Recipe
         fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart',)
 
-    def get_is_favorited(self, queryset, name, value):
+    def get_favorited(self, queryset, name, value):
         if value:
-            return Recipe.objects.filter(favorites__owner=self.request.user)
+            return queryset.filter(favorites__owner=self.request.user)
         return Recipe.objects.all()
 
-    def get_is_in_shopping_cart(self, queryset, name, value):
+    def get_shopping_cart(self, queryset, name, value):
         if value:
-            return Recipe.objects.filter(
-                shopping_cart__owner=self.request.user
-            )
+            return queryset.filter(shopping_cart__owner=self.request.user)
         return Recipe.objects.all()
 
 
